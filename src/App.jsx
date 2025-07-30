a4cb45ca3f3d8ebd015fa9fded9e546e39fb5369
Candi Lilun Chiu <candi.chiu@egym.com>
2025-07-30 17:42:45 +0200

    feat: add UserInput and Results components to handle user input and display results

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+import { useState } from "react";
+
 function App() {
-  return <Header />;
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 5000,
+    expectedReturn: 7,
+    duration: 10,
+  });
+
+  const isValidInput = userInput.duration >= 1;
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevInput) => ({
+      ...prevInput,
+      [inputIdentifier]: +newValue,
+    }));
+  };
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onUserInputChange={handleChange} />
+      {!isValidInput && (
+        <p className="center">
+          Please enter a valid duration (at least 1 year).
+        </p>
+      )}
+      {/* Only render Results if the input is valid */}
+      {isValidInput && <Results userInput={userInput} />}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Results.jsx
@@ -0,0 +1,42 @@
+import { calculateInvestmentResults, formatter } from "../util/investment";
+
+export default function Results({ userInput }) {
+  const calculateResults = calculateInvestmentResults(userInput);
+  console.log(calculateResults);
+  const initialInvestment =
+    calculateResults[0].valueEndOfYear -
+    calculateResults[0].annualInvestment -
+    calculateResults[0].interest;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {calculateResults.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
